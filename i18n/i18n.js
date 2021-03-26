0824049471ce53766679e7f826f11925ee55341a
hmv.soluciones <hmv.soluciones@gmail.com>
2021-03-26 00:22:39 -0600

    first commit

---
+++ i18n/i18n.js
@@ -0,0 +1,24 @@
+
+function switchLanguage(language){    
+    $.ajax({
+        url:"../i18n/"+language+".json",
+        dataType: "json",
+        type:"get"
+    }).done(function(data){  
+        $.each(data, function(id,value ){
+            console.log(id);
+            console.log(value);
+            $("#"+id).html(value);
+        });    
+    });
+
+}
+function processLangDocument(){
+    var tags = document.querySelectorAll('span,img,a,label,li,option,h1,h2,h3,h4,h5,h6');
+    Array.from(tags).forEach(function(value, index){
+        var key = value.dataset.langkey;
+        if(langDocument[key]) value.innerText = langDocument[key];
+    });
+}
+
+switchLanguage("es");
